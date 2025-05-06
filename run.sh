bce2216ba1aaf24624b8a66059d38b3cfa31d2c5
OlgaFilili <philippova.ov@gmail.com>
2025-05-06 18:32:37 +0300

    change run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # подставляем переменные из окружения в подготовленный конфиг
-cat config/heroku_config.yaml | envsubst > config/config.yaml 
+cat config/koyeb-config.yaml | envsubst > config/config.yaml 
 # необходимо для того, чтобы alembic смог найти наше приложение
 export PYTHONPATH=. 
 # обновляем версию базы до последней
