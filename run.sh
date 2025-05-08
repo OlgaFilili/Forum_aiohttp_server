d5d6524f9e866cda1c83e8a3cfb76a61e55bbcfc
OlgaFilili <philippova.ov@gmail.com>
2025-05-08 14:25:53 +0300

    don`t update alembic, what with gino?

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ cat config/koyeb-config.yaml | envsubst > config/config.yaml
 # необходимо для того, чтобы alembic смог найти наше приложение
 export PYTHONPATH=.
 # обновляем версию базы до последней
-alembic upgrade head 
+#alembic upgrade head 
 # запускаем сервер
 python main.py
