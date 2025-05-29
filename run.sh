d08a80998a1a607068b01b92f426fbb8b9fcd2df
OlgaFilili <philippova.ov@gmail.com>
2025-05-29 21:38:10 +0300

    delete docker-compose.yaml from repo

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # подставляем переменные из окружения в подготовленный конфиг
 set -e  # fail on error
-
+if [ ! -f config/koyeb-config.yaml ]; then
+  echo "❌ config/koyeb-config.yaml not found!"
+  exit 1
+fi
 echo "[run.sh] Checking required environment variables..."
 
 : "${DATABASE_USER:?Missing DATABASE_USER}"
 : "${DATABASE_PASSWORD:?Missing DATABASE_PASSWORD}"
 : "${DATABASE_HOST:?Missing DATABASE_HOST}"
 : "${DATABASE_NAME:?Missing DATABASE_NAME}"
-cat config/koyeb-config.yaml | envsubst > config/config.yaml 
+#cat config/koyeb-config.yaml | envsubst > config/config.yaml 
+envsubst < config/koyeb-config.yaml > config/config.yaml
 # необходимо для того, чтобы alembic смог найти наше приложение
 export PYTHONPATH=.
 # обновляем версию базы до последней
