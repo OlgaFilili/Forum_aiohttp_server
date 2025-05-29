b588b031cdbeca1c1265dbb196232588ea1bbd98
OlgaFilili <philippova.ov@gmail.com>
2025-05-29 11:28:17 +0300

    apply chatgpt advices

--- app/store/database/accessor.py
+++ app/store/database/accessor.py
@@ -8,15 +8,16 @@ class PostgresAccessor:
         self.message = Message
         self.db = None
 
-    def setup(self, application: web.Application) -> None:
-        application.on_startup.append(self._on_connect)
-        application.on_cleanup.append(self._on_disconnect)
-
-    async def _on_connect(self, application: web.Application):
-        # здесь был импорт дб
+    async def setup(self, application: web.Application) -> None:
         self.config = application["config"]["postgres"]
         await db.set_bind(self.config["database_url"])
         self.db = db
+        #application.on_startup.append(self._on_connect)
+        application.on_cleanup.append(self._on_disconnect)
+
+    #async def _on_connect(self, application: web.Application):
+        # здесь был импорт дб
+        
 
     async def _on_disconnect(self, _) -> None:
         if self.db is not None:
