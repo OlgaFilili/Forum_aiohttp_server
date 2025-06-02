2fb00852e792033b7e7675fed9b93c9fa529fb0e
OlgaFilili <philippova.ov@gmail.com>
2025-06-02 20:45:51 +0300

    add static routes

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def setup_routes(application):
    # здесь был импорт сетап_роутс
     setup_forum_routes(application)  # настраиваем url-пути приложения forum
    
-    static_dir = Path(__file__).parent / "static"
-    application.router.add_static('/static/', path=static_dir, name='static')
+    static_dir = {BASE_DIR} / "static"
+    application.router.add_static("/static/", path=static_dir, name="static")
 
 def setup_app(application):
    setup_config(application)# добавилось после подключения конфига
